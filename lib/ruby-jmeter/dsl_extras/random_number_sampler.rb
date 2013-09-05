611485d2f0c9a8bc6058b1c4a91b2b0367abe6d7
Carlo Scarioni <carlo.scarioni@simplybusiness.co.uk>
2013-09-05 16:41:18 +0100

    hardcoded uneeded variables

--- lib/ruby-jmeter/dsl_extras/random_number_sampler.rb
+++ lib/ruby-jmeter/dsl_extras/random_number_sampler.rb
@@ -1,18 +1,18 @@
 RANDOM_VARIABLE_SCRIPT = <<RANDOM_NUMBER_VARIABLE
 import java.util.Random;
 
-String[] categories = (vars.get("search_terms")).split(",");
+String[] categories = (vars.get("_for_random_value_")).split(",");
 
 int idx = new Random().nextInt(categories.length);
 String category = (categories[idx]);
 
-vars.put("search_term", category);
+vars.put("final_var_name", category);
 RANDOM_NUMBER_VARIABLE
 
 module RubyJmeter
   class DSL
     def random_number_sampler(params)
-      variables(name: params[:variable], value: params[:variable_values])
+      variables(name: '_for_random_value_', value: params[:variable_values])
       beanshell_sampler({'query' =>  RANDOM_VARIABLE_SCRIPT.gsub('final_var_name', params[:final_variable])})
     end
   end
